969c1865aea2b0f3d62edeb8ce14581a6acba93f
dsanchez <dsanchez@campus-eni.fr>
2025-09-10 11:41:26 +0200

    Module 05 - Tp2 - Fixtures et Faker

---
+++ src/DataFixtures/WishFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Wish;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class WishFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $faker = \Faker\Factory::create('en_EN');
+        for ($i = 0; $i <= 10; $i++) {
+            $wish = new Wish();
+            $wish->setTitle($faker->word);
+            $wish->setAuthor($faker->name);
+            $wish->setDescription($faker->realText());
+            $wish->setDateCreated(\DateTimeImmutable::createFromMutable(
+                $faker->dateTimeBetween('-3 months', 'now')));
+            $wish->setPublished($faker->numberBetween(0, 1));
+            $manager->persist($wish);
+        }
+        $manager->flush();
+    }
+}
