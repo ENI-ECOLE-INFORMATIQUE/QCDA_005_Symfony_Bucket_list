77e9700d570111555d208fbf280073ee35b0ad47
dsanchez <dsanchez@campus-eni.fr>
2025-09-10 11:22:26 +0200

    Module 05 - Tp1 - Données et Doctrine
    Création de l'entité Wish
    Modification du Controller pour utiliser le Repository de Wish
    Modification des templates twig detail et list.

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,16 +10,25 @@ use Symfony\Component\Routing\Attribute\Route;
 final class WishController extends AbstractController
 {
     #[Route('/wishes', name: 'wish_list', methods: ['GET'] )]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('wish/list.html.twig', []);
+        $wishes = $wishRepository->findBy(['published' => true], ['dateCreated' => 'DESC']);
+        return $this->render('wish/list.html.twig', [
+            'wishes' => $wishes
+        ]);
     }
 
     #[Route('/wishes/{id}', name: 'wish_detail', requirements: ['id'=>'\d+'],methods: ['GET'])]
-    public function detail(int $id): Response
+    public function detail(int $id,WishRepository $wishRepository): Response
     {
+        //Récupère ce wish en fonction de l'id présent dans l'URL
+        $wish = $wishRepository->find($id);
+        //Erreur 404 s'il n'existe pas
+        if(!$wish){
+            throw $this->createNotFoundException('Wish not found');
+        }
         return $this->render('wish/detail.html.twig',
-            []
+            ["wish" => $wish]
         );
     }
 }
